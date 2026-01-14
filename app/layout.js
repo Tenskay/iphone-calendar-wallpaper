eb860f01a06ca113e3be6fc6ecc0962ba89a185d
Tenskay <kravcenko.stohi@gmail.com>
2026-01-14 22:02:55 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "iPhone Calendar Wallpaper"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
